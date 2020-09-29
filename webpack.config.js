712c1b288d1b96a3a3c2cbdf8224615483131167
swimjiy <swimjiy@pubg.com>
2020-09-29 15:34:33 +0900

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -30,7 +30,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [{
           loader: 'babel-loader',
@@ -41,13 +41,6 @@ module.exports = {
             ]
           }
         }]
-      },
-      {
-        test: /\.jsx?/,
-        loader: 'babel-loader',
-        options: {
-          presets: [['@babel/preset-env'], ['@babel/preset-react']],
-        },
       },
     ],
   },
